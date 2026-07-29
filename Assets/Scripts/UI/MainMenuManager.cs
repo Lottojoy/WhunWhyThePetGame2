33f536c23bce6c194a504ac662ab5b556070ea99
team5131naja <129346353+team5131naja@users.noreply.github.com>
2026-07-29 22:37:37 +0700

    add optionsMenu
    เพิ่มระบบ volume setting เสียง bg music
    bg music เพื่อการทดลองเฉยๆ

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject optionsMenu;
+    [Header("--------- MenuCanvas ---------")]
+    [SerializeField] private GameObject generalSettingMenu;
+    [SerializeField] private GameObject controlsMenu;
 
+    [Header("--------- MainButton ---------")]
     [SerializeField] private Button optionsBtn;
+
+    [Header("--------- ChangePageButton ---------")]
+    [SerializeField] private Button settingMenuBtn;
+    [SerializeField] private Button controlsMenuBtn;
 
     private GameObject currentMenu;
 
@@ -20,10 +25,37 @@ public class MainMenuManager : MonoBehaviour
     void Start()
     {
         optionsBtn.onClick.AddListener(() =>
-                {
-                    currentMenu = optionsMenu;
-                    currentMenu.SetActive(true);
-                });
+        {
+            currentMenu = generalSettingMenu;
+            currentMenu.SetActive(true);
+        });
+        settingMenuBtn.onClick.AddListener(() =>
+        {
+            if (currentMenu)
+            {
+                currentMenu.SetActive(false);
+            }
+            currentMenu = generalSettingMenu;
+            currentMenu.SetActive(true);
+        });
+        controlsMenuBtn.onClick.AddListener(() =>
+        {
+            if (currentMenu)
+            {
+                currentMenu.SetActive(false);
+            }
+            currentMenu = controlsMenu;
+            currentMenu.SetActive(true);
+        });
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
     }
 
     // Update is called once per frame
